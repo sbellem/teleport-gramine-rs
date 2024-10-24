760ece3efd265b5b38ddf64a7e4129b43760b7f7
Sylvain Bellemare <sbellem@gmail.com>
2024-10-24 13:09:20 -0700

    nix: wip

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?rev=63dacb46bf939521bdc93981b4cbb7ecb58427a0";
     rust-overlay.url = "github:oxalica/rust-overlay?rev=29b1275740d9283467b8117499ec8cbb35250584";
+    nix-filter.url = "github:numtide/nix-filter?rev=3342559a24e85fc164b295c3444e8a139924675b";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, nix-filter }:
     let
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
@@ -21,6 +22,7 @@
           ];
         };
       });
+      filter = nix-filter.lib;
     in
     {
       overlays.default = final: prev: {
@@ -40,7 +42,20 @@
             name = "teleport";
             version = "0.1.0";
             #src = gitignoreSource extraIgnores ./.;
-            src = ./.;
+            src = filter {
+              root = ./.;
+              include = [
+                ./Cargo.toml
+                ./Cargo.lock
+                ./rust-toolchain
+                ./src
+                ./abi
+                ./templates
+              ];
+              exclude = [
+                ./src/bin/redeem.rs
+              ];
+            };
             doCheck = false;
             cargoLock = {
               lockFile = ./Cargo.lock;
